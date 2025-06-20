9e47a2b498fb80b727335f228ccc1d2ced434210
Evgeniy Potapov <41373877+jakcpto@users.noreply.github.com>
2025-06-20 12:35:40 +0400

    Comment API for testing

--- rlama_create.sh
+++ rlama_create.sh
@@ -2,5 +2,7 @@
 curl -fsSL https://raw.githubusercontent.com/dontizi/rlama/main/install.sh | sudo sh
 rlama install-dependencies
 rlama rag hf.co/TECCOD/adilet-llama-8b-250619-ollama adilet ./rlama_docs
-rlama api adilet
+# for API run next command
+#rlama api adilet
+# for manual tests run this command
 rlama run adilet
